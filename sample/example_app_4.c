1eb433488de9495a41151c5e373cb14f41add6d9
Tim Jammer <tim.jammer@tu-darmstadt.de>
2025-01-23 13:55:14 +0100

    added testcases, where library is loaded after MPI init

---
+++ sample/example_app_4.c
@@ -0,0 +1,40 @@
+
+#include <stdio.h>
+#include <assert.h>
+#include <dlfcn.h>
+
+// content of "wrapper_lib.h" but with weak linkeage
+
+__attribute__((weak)) void perform_operation();
+typedef void (*func_ptr_type)();
+
+//#define LIBRARY_TO_LOAD to the library that should be dlopened
+
+#include "mpi_lib.h"
+
+#ifndef TAG_TO_USE
+#define TAG_TO_USE 42
+#endif
+
+int main(int argc, char **argv) {
+
+    sample_mpi_init(NULL,NULL);
+
+    printf("dlopen: %s\n",LIBRARY_TO_LOAD);
+
+    // dynamically open the library
+    void* handle = dlopen(LIBRARY_TO_LOAD, RTLD_NOW);
+    func_ptr_type perform = dlsym(handle, "perform_operation");
+
+    sample_mpi_recv(TAG_TO_USE);
+
+    if (perform != NULL) {
+        perform();
+    } else {
+        printf("Failed to dlopen the correct library\n");
+    }
+
+    dlclose(handle);
+
+    sample_mpi_finalize();
+}
