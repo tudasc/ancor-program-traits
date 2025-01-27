19e0e553bea69a79fad62c4ad118b38382bd200b
Tim Jammer <tim.jammer@tu-darmstadt.de>
2025-01-27 16:14:27 +0100

    added mpi hello world

---
+++ sample/pmpi_wrapper_test/mpi_hello_world.c
@@ -0,0 +1,19 @@
+/*The Parallel Hello World Program*/
+#include <stdio.h>
+#include <mpi.h>
+
+// compile /home/tim/mpich/install/bin/mpicc mpi_hello_world.c
+// run LD_PRELOAD=/home/tim/ancor-programm-traits/cmake-build-debug/home/tim/mpich/install/bin/mpirun -n 2 ./a.out
+// without LD_PRELOAD: our mpi wrapper checking for wildcard usage is not used
+
+int main(int argc, char **argv)
+{
+    int node;
+
+    MPI_Init(&argc,&argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &node);
+
+    printf("Hello World from Node %d\n",node);
+
+    MPI_Finalize();
+}
